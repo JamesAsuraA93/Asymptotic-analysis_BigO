d350dad82192e04148895daff96f0ffe6eab2753
AsuraA93 <56329129+JamesAsuraA93@users.noreply.github.com>
2021-09-13 21:58:35 +0700

    Update O(N²).py
    Add bubble sorting

--- O(N²).py
+++ O(N²).py
@@ -4,5 +4,24 @@ def display_all(lst):
             print([item1, item2], end=" ")
         print("\r")
 
+def bubble_sorting():
+    lst = [5,2,5,25,1,5,35,323,12,4,125,25,13,5,12,5,15,13,42]
 
+    rng = len(lst)
+
+    for i in range(rng):
+
+        for j in range(0,len(lst)-1):
+
+            if lst[j] > lst[j+1]:
+                lst[j],lst[j+1] = lst[j+1],lst[j]
+            else:
+                pass
+        rng -= 1
+
+    print(lst)
+    
+    
 display_all([1, 2, 3, 4, 5])
+
+bubble_sorting()
